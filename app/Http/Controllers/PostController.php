d8378a6c333525dae77f3031815c6c5b107f3812
kimitakanabe <kimiko.takanabe@gmail.com>
2025-06-26 17:54:32 +0200

    Create&Store action

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,4 +21,23 @@ class PostController extends Controller{
         // 変数$postをposts/show.blade.phpファイルに渡す
         return view('posts.show', compact('post'));
     }
+
+    public function create(){
+        return view('posts.create');
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'title' => 'required|max:20',
+            'content' => 'required|max:200'
+        ]);
+
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+
+        return redirect('/posts');
+
+    }
 }
